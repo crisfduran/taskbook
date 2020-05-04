8648448f1937f83d73234a90143800cd45806f09
Cris Duran <cris@franssonduran.se>
2020-05-04 12:27:53 +0200

    Add CMB2 plugin. Downloaded plugin from github repo, put needed functions in own file outside of plugin folder. Read more at https://cmb2.io

--- taskbook.php
+++ taskbook.php
@@ -29,7 +29,6 @@ along with Task Book. If not, see https://www.gnu.org/licenses/gpl.html.
 require_once plugin_dir_path( __FILE__ ) . 'includes/posttypes.php';
 register_activation_hook( __FILE__, 'taskbook_rewrite_flush' );
  
-
 /**
  * Register Task Logger role.
  */
@@ -37,16 +36,18 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/roles.php';
 register_activation_hook( __FILE__, 'taskbook_register_role' );
 register_deactivation_hook( __FILE__, 'taskbook_remove_role' );
 
-
 /**
  * Add capabilities.
  */
 register_activation_hook( __FILE__, 'taskbook_add_capabilities' );
 register_deactivation_hook( __FILE__, 'taskbook_remove_capabilities' );
 
-
 /**
  * Register task_status field.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/status.php';
 
+/**
+ * Register CMB2 metaboxes and fields.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/CMB2-functions.php';
